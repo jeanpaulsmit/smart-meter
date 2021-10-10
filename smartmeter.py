bd57bb532589d30ee174eb4c6ef1b598724c3516
Jean-Paul Smit <github@didago.nl>
2021-10-10 20:44:52 +0200

    Add files via upload

---
+++ smartmeter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import time
+from datetime import datetime
+import argparse
+import json
+import sys
+import serial
+
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.bytesize=serial.SEVENBITS
+ser.parity=serial.PARITY_EVEN
+ser.stopbits=serial.STOPBITS_ONE
+ser.xonxoff=0
+ser.rtscts=0
+ser.timeout=20
+ser.port="/dev/ttyUSB0"
+
+try:
+    ser.open()
+except:
+    sys.exit ("Error opening %s. Program stopped."  % ser.name)
+
+p1_counter =0
+stack=[]
+
+while p1_counter < 36:
+    p1_line=''
+    try:
+        p1_raw = ser.readline()
+    except:
+        sys.exit ("Serial port %s can't be read. Program stopped." % ser.name )
+    p1_str=str(p1_raw)
+    p1_line=p1_str.strip()
+    stack.append(p1_line)
+    p1_counter = p1_counter +1
+
+# print stack
+
+# Send data to azure queue for further processing
+from azure.storage.queue import QueueClient
+queue = QueueClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=smartmeterreadings;AccountKey=cOWMFhFEif44qeuQsk5um+PtK0OdSv2pWjisigL1f0ecedyBO1vwdNv1WQdC3LqTY5o4hPeJUFpnzKhs2pnGuA==;EndpointSuffix=core.windows.net", queue_name="readings")
+queue.send_message(stack)
+
+try:
+    ser.close()
+except:
+    sys.exit ("Ohoh %s. Program stopped." % ser.name )
